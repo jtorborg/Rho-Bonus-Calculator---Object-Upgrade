4c6e4f97f949320197313d192e12ed24157b758d
Jacquelyn <jtorborg1227@comcast.net>
2016-09-22 22:34:45 -0500

    finished

--- group_01.js
+++ group_01.js
@@ -1,15 +1,11 @@
-var atticus = ["Atticus", "2405", "47000", 3];
-var jem = ["Jem", "62347", "63500", 4];
-var boo = ["Boo", "11435", "54000", 3];
-var scout = ["Scout", "6243", "74750", 5];
-
-var employees = [atticus, jem, boo, scout];
-
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
 
 
 //Constructor
 function Person(name, employeeNum, salary, rating) {
-
     this.name = name;
     this.employeeNum = employeeNum;
     this.salary = salary;
@@ -21,23 +17,63 @@ var jem = new Person("Jem", "62347", "63500", 4);
 var boo = new Person("Boo", "11435", "54000", 3);
 var scout = new Person("Scout", "6243", "74750", 5);
 
-
+var employees = [atticus, jem, boo, scout];
 console.log(atticus);
 console.log(jem);
 console.log(boo);
 console.log(scout);
 
-function calcBonus(person) {
-    var employeeName = person.name;
-    var employeeNumber = person.employeeNum;
-    var employeeSalary = person.salary;
-    var employeeRating = person.rating;
-    var employeeBonus;
 
-    console.log('Employee Name:', employeeName);
-    console.log('Employee Number:', employeeNumber);
-    console.log('Employee Salary:', employeeSalary);
-    console.log('Employee Rating:', employeeRating);
+
+for (var i = 0; i < employees.length; i++) {
+    var report = employeeReport(employees);
+}
+
+//function that takes in a single employee
+function employeeReport(employee) {
+    //assign index for employee name to variable
+    var employeeName = employee[i].name;
+    console.log(employeeName);
+    //assign index for employee id to variable
+    var employeeId = employee[i].employeeNum;
+    console.log(employeeId);
+    //assign index for employee salary to variable
+    var employeeSalary = parseInt(employee[i].salary);
+    console.log(employeeSalary);
+    //assign index for employee rating to variable
+    var employeeRating = employee[i].rating;
+    console.log(employeeRating);
+    var bonus = 0;
+    //
+    console.log("Employee Name", employeeName);
+    console.log("Employee ID", employeeId);
+    console.log("Employee Salary", employeeSalary);
+    console.log("Employee Rating", employeeRating);
+
+    //returns another array
+    var newArray = [];
+
+    //first index should also contain the employee's name
+    newArray[0] = employeeName;
+
+    //second index should contain the bonus percentage
+    bonus = calcBonus(employeeId, employeeSalary, employeeRating);
+
+    newArray[1] = bonus;
+
+    //third index should be adjusted annual compensation (base annual + bonus)
+    newArray[2] = employeeSalary + (employeeSalary * employeeBonusPercent);
+    //fourth index should be the employee's total bonus rounded to nearest dollar
+    newArray[3] = Math.round(employeeSalary * employeeBonusPercent);
+    console.log(newArray);
+    return newArray;
+
+}
+
+function calcBonus(employeeId, employeeSalary, employeeRating) {
+
+    var employeeBonus = 0;
+
 
     switch (employeeRating) {
         case 0:
@@ -47,33 +83,29 @@ function calcBonus(person) {
         case 2:
             break;
         case 3:
-            employeeBonus = .04;
+            employeeBonusPercent = .04;
             break;
         case 4:
-            employeeBonus = .06;
+            employeeBonusPercent = .06;
             break;
         case 5:
-            employeeBonus = .1;
+            employeeBonusPercent = .1;
             break;
         default:
-            console.log("I have no idea what the bonus is.");
+            console.log("No bonus.");
             break; //break statement optional on default case
     }
 
-    if (employeeNumber.length <= 4) {
-        employeeBonus = employeeBonus + .05;
+    if (employeeId.length <= 4) {
+        employeeBonusPercent = employeeBonusPercent + .05;
     }
     if (employeeSalary > 65000) {
-        employeeBonus = employeeBonus - .01;
+        employeeBonusPercent = employeeBonusPercent - .01;
     }
-    if (employeeBonus >= .13) {
-        employeeBonus = 0.13;
+    if (employeeBonusPercent >= 0.13) {
+        employeeBonusPercent = 0.13;
     }
-    console.log("Employee Bonus", employeeBonus);
-}
+    console.log("Employee Bonus Percent", employeeBonusPercent);
+    return employeeBonusPercent;
 
-//iterate through array and call function
-calcBonus(atticus);
-calcBonus(jem);
-calcBonus(boo);
-calcBonus(scout);
+}
